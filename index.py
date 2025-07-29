7f73ba00d8fcef25d96ae6162fa3c21177804e5e
Abdellah-D-Prog <djezzar.abdellah@hotmail.com>
2025-07-29 15:14:56 +0200

    feat:database set up ok

--- index.py
+++ index.py
@@ -4,11 +4,14 @@ import uvicorn
 #import docker 
 from pydantic import BaseModel
 
+app=FastAPI(title="Python Scout")
+
 
 class Scout(BaseModel):
     name:str
     age:int
     group_name:str
+    
 class Camp(BaseModel):
     name:str
     location:str
