a7fba6f79c0d6ef436125dd27ce94b318e953229
Abdellah-D-Prog <djezzar.abdellah@hotmail.com>
2025-07-31 09:17:47 +0200

    feat:sqlAlchemy installed and root endpoint ok

--- index.py
+++ index.py
@@ -17,6 +17,17 @@ app=FastAPI(title="Scout API",
 app.include_router(scoots.router,prefix="/scoots",tags=["/scoots"])
 app.include_router(camps.router,prefix="/camps",tags=["/camps"])
 
+#Endpoint racine
+@app.get("/")
+def root():
+    return{"message":"Bienvenue sur l'API Scout!",
+           "description":"C'est une API de gestion de jeunes scouts et camps scouts",
+           "documentation":"/docs",
+           "Les endpoints": {
+               "scouts":"/scoots",
+               "camps":"/camps"
+           }}
+
 if __name__ == "__main__":
     print("Démarrage de l'API Python scoot...")
     uvicorn.run(app, host="localhost", port=8000)
